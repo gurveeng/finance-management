992e339760ac9e8726e8c30c3cb3814158856421
gurveeng <gurveenngill@gmail.com>
2024-08-29 20:40:26 -0700

    add home page ui layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import HeaderBox from "@/components/HeaderBox"
 
 const Home = () => {
+  const loggedIn = {firstName: 'Gurveen'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Acess and manage your account and transactions."
+          />
+
+          <TotalBalanceBox  
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1200.50}>
+
+          </TotalBalanceBox>
+
+        </header>
+      </div>
+    </section>
   )
 }
 
